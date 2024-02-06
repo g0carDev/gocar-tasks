394e2e0fd8b745572a8715061cc75974f867d8ba
Jovan Gómez <111164889+g0carDev@users.noreply.github.com>
2024-02-06 14:22:32 -0600

    Add custom fonts

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,10 +1,35 @@
-import { Button } from "@/components/ui/button";
-import { Medal } from "lucide-react";
 import Link from "next/link";
+import localFont from "next/font/local";
+import { Poppins } from "next/font/google";
+import { Medal } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+
+const headingFont = localFont({
+  src: "../../public/fonts/font.woff2"
+});
+
+const textFont = Poppins({
+  subsets: ["latin"],
+  weight: [
+    "100",
+    "200",
+    "300",
+    "400",
+    "500",
+    "600",
+    "700",
+    "800",
+    "900"
+  ],
+});
 
 export default function MarketingPage() {
   return <div className="flex items-center justify-center flex-col">
-    <div className="flex items-center justify-center flex-col">
+    <div className={cn(
+      "flex items-center justify-center flex-col",
+      headingFont.className,
+    )}>
       <div className="mb-4 flex items-center border shadow-sm p-4
       rounded-full bg-amber-100 text-amber-700 uppercase
       ">
@@ -16,8 +41,11 @@ export default function MarketingPage() {
         work forward.
       </div>
     </div>
-    <div className="text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto text-balance">
-      Collaborate,, manage projects, and reach new productivity peaks. From high rises to the home office, the way your team works is unique—accomplish it all with G0C4R TASKS.
+    <div className={cn(
+      "text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto",
+      textFont.className,
+    )}>
+      Collaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the way your team works is unique—accomplish it all with G0C4R TASKS.
     </div>
     <Button className="mt-6" size="lg" asChild>
       <Link href='/sign-up'>Get GOCAR TASKS for free</Link>
